48ce31ca0f73845633f266b77cdaab3bf2857a0a
poiugit <poiuwk@gmail.com>
2020-10-22 18:15:15 +0900

    JS: project Filtering

--- main.js
+++ main.js
@@ -50,10 +50,36 @@ document.addEventListener('scroll', () => {
   }
 })
 
+
 // Handle click on the "arrow up" uptton
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 })
+
+
+// Filter projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
+
 
 
 function scrollIntoView(selector) {
